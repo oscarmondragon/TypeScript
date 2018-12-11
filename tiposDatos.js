4ec21b2057b807fb0b4bc529d7c28080f2b22625
Oscar <oscarmondragon100@gmail.com>
2018-12-11 08:57:04 -0600

    First Commit

---
+++ tiposDatos.js
@@ -0,0 +1,14 @@
+"use strict";
+var esSuperman = true;
+var esBatman;
+var esAcuaman = true;
+if (esSuperman) {
+    console.log("Estamos salvados");
+}
+else {
+    console.log("Opppps! ohh");
+}
+esSuperman = convertirClark();
+function convertirClark() {
+    return false;
+}
---
+++ tiposDatos.ts
@@ -0,0 +1,21 @@
+
+let esSuperman:boolean = true;
+let esBatman:boolean;
+let esAcuaman:boolean = true;
+
+
+if(esSuperman){
+    console.log("Estamos salvados");
+
+} else {
+    console.log("Opppps! ohh");
+}
+
+esSuperman = convertirClark();
+
+
+function convertirClark(){
+    return false;
+}
+
+
